f86c69c830cdac1eaf93e6b2ab8ad07292e59766
markydokdok <47651607+markydokdok@users.noreply.github.com>
2019-03-24 13:58:37 +0800

    Update pitextreader.py

--- pitextreader.py
+++ pitextreader.py
@@ -1,46 +1,26 @@
-#!/usr/bin/python
-# 
-# PiTextReader - Raspberry Pi Printed Text-to-Speech Reader
-#
-# Allows sight impaired person to have printed text read using
-# OCR and text-to-speech.
-#
-# Normally run by pi crontab at bootup
-# Turn off by commenting out @reboot... using $ crontab -e; sudo reboot
-# Manually run using $ python pitextreader.py
-#
-# This is a simplistic (i.e. not pretty) python program
-# Just runs cmd-line pgms raspistill, tesseract-ocr, flite to do all the work
-#
-# Version 1.0 2018.02.10 - initial release - rgrokett
-# v1.1 - added some text cleanup to improve reading
-#
-# http://kd.grokett.com/
-#
-# License: GPLv3, see: www.gnu.org/licenses/gpl-3.0.html
-#
- 
 import RPi.GPIO as GPIO
 import os, sys
 import logging
 import subprocess
 import threading
-import time 
+import time
+import datetime as dt
+import shutil
 
+#Ito yung unique number based sa time and
+fileNameNya = dt.datetime.now().isoformat()
 
 ##### USER VARIABLES
-DEBUG	= 0	# Debug 0/1 off/on (writes to debug.log)
+DEBUG   = 0 # Debug 0/1 off/on (writes to debug.log)
 SPEED   = 0.8   # Speech speed, 0.5 - 2.0 
-VOLUME  = 190	# Audio volume
-
-# OTHER SETTINGS
-SOUNDS  = "/home/pi/PiTextReader/sounds/" # Directory for sound effect(s)
-CAMERA  = "raspistill -cfx 128:128 --awb auto -rot 180 -t 500 -o /tmp/image.jpg"
+VOLUME  = 100    # Audio volume
 
 # GPIO BUTTONS
-BTN1	= 24	# The button!
-LED	= 18	# The button's LED!
+BTN1    = 24    # The button!
+LED     = 18    # The button's LED!
 
+def doesFileExist(filePathAndName):
+    return os.path.exists(filePathAndName)
 
 ### FUNCTIONS
 # Thread controls for background processing
@@ -62,7 +42,7 @@ class RaspberryThread(threading.Thread):
         self.running = False 
 
 # LED ON/OFF
-def led(val):	
+def led(val):   
     logger.info('led('+str(val)+')') 
     if val:
        GPIO.output(LED,GPIO.HIGH)
@@ -81,7 +61,7 @@ def sound(val): # Play a sound
 # SPEAK STATUS
 def speak(val): # TTS Speak
     logger.info('speak()') 
-    cmd = "/usr/bin/flite -voice awb --setf duration_stretch="+str(SPEED)+" -t \""+str(val)+"\""
+    cmd = "/usr/bin/flite -voice slt --setf duration_stretch="+str(SPEED)+" -t \""+str(val)+"\""
     logger.info(cmd) 
     os.system(cmd)
     return 
@@ -98,7 +78,8 @@ def volume(val): # Set Volume for Launch
 # TEXT CLEANUP
 def cleanText():
     logger.info('cleanText()')
-    cmd = "sed -e 's/\([0-9]\)/& /g' -e 's/[[:punct:]]/ /g' -e 'G' -i /tmp/text.txt"
+    #cmd = "sed -e 's/\([0-9]\)/& /g' -e 's/[[:punct:]]/ /g' -e 'G' -i /tmp/text.txt"
+    cmd = "sed -e 's/\([0-9]\)/& /g' -e 's/[[:punct:]]/ /g' -e 'G' -i /tmp/" + fileNameNya + ".txt"
     logger.info(cmd) 
     os.system(cmd)
     return
@@ -107,7 +88,8 @@ def cleanText():
 def playTTS():
     logger.info('playTTS()') 
     global current_tts
-    current_tts=subprocess.Popen(['/usr/bin/flite','-voice','awb','-f', '/tmp/text.txt'],
+    #current_tts=subprocess.Popen(['/usr/bin/flite','-voice','slt','-f', '/tmp/text.txt'],
+    current_tts=subprocess.Popen(['/usr/bin/flite','-voice','slt','-f', '/tmp/' + fileNameNya + '.txt'],
         stdin=subprocess.PIPE,stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,close_fds=True)
     # Kick off stop audio thread 
@@ -122,7 +104,7 @@ def stopTTS():
     global current_tts
     # If button pressed, then stop audio
     if GPIO.input(BTN1) == GPIO.LOW:
-	logger.info('stopTTS()') 
+    #logger.info('stopTTS()') 
         #current_tts.terminate()
         current_tts.kill()
         time.sleep(0.5)
@@ -131,23 +113,25 @@ def stopTTS():
 # GRAB IMAGE AND CONVERT
 def getData():
     logger.info('getData()') 
-    led(0) # Turn off Button LED
+    #led(1) # Turn off Button LED
 
     # Take photo
-    sound(SOUNDS+"shutter.wav")
+    sound(SOUNDS+"camera-shutter.wav")
     cmd = CAMERA
     logger.info(cmd) 
     os.system(cmd)
 
     # OCR to text
-    speak("processing. please wait.")
-    cmd = "/usr/bin/tesseract /tmp/image.jpg /tmp/text"
+    speak("Now Reading. Please, be Patient.")
+    led(0)
+    #cmd = "/usr/bin/tesseract /tmp/image.jpg /tmp/text"
+    cmd = "/usr/bin/tesseract /tmp/" + fileNameNya + ".jpg /tmp/" + fileNameNya
     logger.info(cmd) 
     os.system(cmd)
     
     # Cleanup text
     cleanText()
-
+    led(0)
     # Start reading text
     playTTS()
     return
@@ -184,23 +168,63 @@ try:
     rt = RaspberryThread( function = stopTTS ) # Stop Speaking text
     
     volume(VOLUME)
-    speak("OK, ready, go")
-    led(1)
+    speak("The Device is Ready to use")
+    
     
     while True:
         if GPIO.input(BTN1) == GPIO.LOW:
-            # Btn 1
-	    getData()
-            rt.stop()
-    	    rt = RaspberryThread( function = stopTTS ) # Stop Speaking text
             led(1)
-	    time.sleep(0.5)	 
-            speak("OK, ready, go")
-	time.sleep(0.2)	 
+            # OTHER SETTINGS
+            SOUNDS  = "/home/pi/PiTextReader/sounds/" # Directory for sound effect(s)
+            #CAMERA  = "raspistill -cfx 128:128 --awb auto -rot 180 -t 500 -o /tmp/image.jpg"
+            CAMERA  = "raspistill -cfx 128:128 --awb auto -rot 180 -t 500 -o /tmp/" + fileNameNya + ".jpg"
+            flashDrive = "/media/pi/TOPIX/"
+            
+            # Btn 1
+            getData()
+            rt.stop()
+            rt = RaspberryThread( function = stopTTS ) # Stop Speaking text
+            #led(0)
+            
+            time.sleep(1.0)
+                
+            if doesFileExist(flashDrive):
+                picSource = "/tmp/%s.jpg" % fileNameNya
+                textSource = "/tmp/%s.txt" % fileNameNya
+                
+                
+                #picDestination = "/media/pi/TOPIX/%s.jpg" % fileNameNya
+                #textDestination = "/media/pi/TOPIX/%s.txt" % fileNameNya
+                a1 = "/media/pi/TOPIX/%s.jpg" % fileNameNya
+                a2 = "/media/pi/TOPIX/%s.txt" % fileNameNya
+                
+                b1 = a1.replace(':','')
+                c1 = b1.replace('-','')
+                
+                b2 = a2.replace(':','')
+                c2 = b2.replace('-','')
+                
+                try:
+                    # Copy file to destination
+                    shutil.copy2(picSource, c1)
+                    shutil.copy2(textSource, c2)
+                   # E.g. source and destination is the same location
+                except shutil.Error as e:
+                    print("Error: %s" % e)
+                    # E.g. source or destination does not exist
+                except IOError as e:
+                    print("Error: %s" % e.strerror)
+                    
+                fileNameNya = dt.datetime.now().isoformat()
+                speak("Finish Reading, and Finish Saving to USB. The Device is Ready to use again")
+                time.sleep(0.2)
+            else:
+                fileNameNya = dt.datetime.now().isoformat()
+                speak("Finish Reading. The Device is Ready to use again")
+                time.sleep(0.2)
     
 except KeyboardInterrupt:
     logger.info("exiting")
 
 GPIO.cleanup() #Reset GPIOs
 sys.exit(0)
-
